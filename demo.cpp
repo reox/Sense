629463db96e00a6f7525f9f2544c700662b8e351
Sebastian Bachmann <me@free-minds.net>
2010-10-01 16:31:25 +0200

    demo with analog input

--- demo.cpp
+++ demo.cpp
@@ -6,16 +6,21 @@
 void setup() {
 	Serial.begin(19200);
 	pinMode(PIN, OUTPUT);
+	pinMode(8, OUTPUT);
+	digitalWrite(8, LOW);
 	digitalWrite(PIN, HIGH);
 }
 
 void loop() {
-	delay(TIME);
+	int strobe_delay = analogRead(5) / 10;
+	delay(strobe_delay);
 	digitalWrite(PIN, LOW);
-	delay(TIME);
+	delay(strobe_delay);
 	digitalWrite(PIN, HIGH);
 
 	int x = Serial.available() ? Serial.read() : 0;
+	if(x == 0)
+		digitalWrite(8, HIGH);
 	Serial.write("Hi");
 	Serial.write(x);
 	Serial.write("!\n");
